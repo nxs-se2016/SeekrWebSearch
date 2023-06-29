fc6f28ea5ae6cbdf850369a182e7910a7639f5f5
Nisai S <101230270+nxs-se2016@users.noreply.github.com>
2023-06-29 12:53:33 +0000

    Create config.php
    Used to keep the configuration file separate from the other php files.

---
+++ prototype/config.php
@@ -0,0 +1,8 @@
+<?php
+    // Use this file for configuring the search engine, away from the other php files.
+    
+    $servername = "LOCALHOST";
+    $username = "USERNAME";
+    $password = "PASSWORD";
+    $database = "DATABASE";
+?>
